f5b276094d41fcba479a306c450ccd13fcb28f50
Exybore <exybore@gmail.com>
2019-06-21 20:37:22 +0200

    feat: fixed buttons
    Fixed buttons are here ! Combined with the floating option, it lets the user create FABs !

--- src/components/VBtn/VBtn.svelte
+++ src/components/VBtn/VBtn.svelte
@@ -7,9 +7,11 @@
   export let dark = false;
   export let depressed = false;
   export let disabled = false;
+  export let fixed = false;
   export let flat = false;
   export let floating = false;
-  export let href = "";
+  export let href = "#!";
+  export let target = "";
   export let icon = false;
   export let large = false;
   export let left = false;
@@ -18,11 +20,10 @@
   export let round = false;
   export let small = false;
   export let top = false;
-
-  let tag = href === "" ? "div" : "a";
+  let light = dark ? false : true;
 </script>
 
-<div
+<a
   class="v-btn {color}"
   class:v-btn--absolute={absolute}
   class:v-btn--block={block}
@@ -30,6 +31,7 @@
   class:v-btn--column={column}
   class:v-btn--depressed={depressed}
   class:v-btn--disabled={disabled}
+  class:v-btn--fixed={fixed}
   class:v-btn--flat={flat}
   class:v-btn--floating={floating}
   class:v-btn--icon={icon}
@@ -40,9 +42,11 @@
   class:v-btn--round={round}
   class:v-btn--small={small}
   class:v-btn--top={top}
+  class:theme--light={light}
   class:theme--dark={dark}
-  {href}>
+  {href}
+  {target}>
   <div class="v-btn__content">
     <slot />
   </div>
-</div>
+</a>
