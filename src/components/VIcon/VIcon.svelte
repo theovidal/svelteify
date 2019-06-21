3031db8f346f9ef2d32911d4738992571c95cc8e
Exybore <exybore@gmail.com>
2019-06-21 18:54:00 +0200

    feat: icon component
    Added the VIcon component, which lets the user put really nice Material icons in its app.

---
+++ src/components/VIcon/VIcon.svelte
@@ -0,0 +1,35 @@
+<script>
+  export let color = "";
+  export let dark = false;
+  export let large = false;
+  export let left = false;
+  export let medium = false;
+  export let right = false;
+  export let small = false;
+  export let size = undefined;
+  export let xlarge = false;
+  let light = dark ? false : true;
+
+  if (size === undefined) {
+    if (small) {
+      size = 16;
+    } else if (medium) {
+      size = 28;
+    } else if (large) {
+      size = 36;
+    } else if (xlarge) {
+      size = 40;
+    }
+  }
+</script>
+
+<i
+  class="v-icon material-icons {color}"
+  aria-hidden="true"
+  class:v-icon--left={left}
+  class:v-icon--right={right}
+  class:theme--dark={dark}
+  class:theme-light={light}
+  style={size === undefined ? '' : `font-size: ${size}px`}>
+  <slot />
+</i>
---
+++ src/components/VIcon/index.js
@@ -0,0 +1,4 @@
+import VIcon from './VIcon.svelte'
+
+export { VIcon }
+export default VIcon
