5e6fda7184d6ff685716018e15334dfc09d79f74
Exybore <exybore@gmail.com>
2019-06-23 21:10:03 +0200

    refactor: project rename
    The project was renamed from 'sveltify' to 'svelteify', because the old name is already took.

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,51 @@
-import svelte from 'rollup-plugin-svelte';
+import svelte from 'rollup-plugin-svelte'
 import css from 'rollup-plugin-css-porter'
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
 
-const production = !process.env.ROLLUP_WATCH;
+const production = !process.env.ROLLUP_WATCH
 
 export default {
-	input: 'src/index.js',
-	output: {
-		sourcemap: true,
-		format: 'cjs',
-		name: 'app',
-		file: 'dist/sveltify.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file  better for performance
-			css: css => {
-				css.write('dist/sveltify.css');
-			}
-		}),
-		css({
-      raw: false,
-      'minified': 'dist/sveltify.css'
+  input: 'src/index.js',
+  output: {
+    sourcemap: true,
+    format: 'cjs',
+    name: 'app',
+    file: 'dist/svelteify.js'
+  },
+  plugins: [
+    svelte({
+      // enable run-time checks when not in production
+      dev: !production,
+      // we'll extract any component CSS out into
+      // a separate file  better for performance
+      css: css => {
+        css.write('dist/svelteify.css')
+      }
     }),
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration 
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({ browser: true }),
+    css({
+      raw: false,
+      minified: 'dist/svelteify.css'
+    }),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration 
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve({ browser: true }),
     commonjs(),
 
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload('public'),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
-};
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
