22e2bd0f58cf36136cec7d091c08d9bbcb92360e
Exybore <exybore@becauseofprog.fr>
2019-08-19 18:46:31 +0200

    feat(docs): add icon doc

---
+++ packages/docs/src/components/Icons/Examples.svelte
@@ -0,0 +1,50 @@
+<script>
+  import { MIcon, MLayout } from 'svelteify'
+</script>
+
+Check out the
+<a href="https://material.io/icons/" target="_blank">Material Icons page</a> for
+a full list of icons.
+
+<h2>Icon sizes</h2>
+
+<p>You can set the icon size of your choice according to your needs.</p>
+
+<MLayout row>
+  <MIcon small>home</MIcon>
+  <MIcon small>event</MIcon>
+  <MIcon small>info</MIcon>
+</MLayout>
+<MLayout row>
+  <MIcon>home</MIcon>
+  <MIcon>event</MIcon>
+  <MIcon>info</MIcon>
+</MLayout>
+<MLayout row>
+  <MIcon medium>home</MIcon>
+  <MIcon medium>event</MIcon>
+  <MIcon medium>info</MIcon>
+</MLayout>
+<MLayout row>
+  <MIcon large>home</MIcon>
+  <MIcon large>event</MIcon>
+  <MIcon large>info</MIcon>
+</MLayout>
+<MLayout row>
+  <MIcon xlarge>home</MIcon>
+  <MIcon xlarge>event</MIcon>
+  <MIcon xlarge>info</MIcon>
+</MLayout>
+
+<h2>Colored icons</h2>
+
+<p>
+  The icons are colorable using the helpers. If you want to color the icon
+  itself, use the text mode.
+</p>
+
+<MLayout row>
+  <MIcon color="green--text">home</MIcon>
+  <MIcon color="amber--text">event</MIcon>
+  <MIcon color="light-blue--text">info</MIcon>
+</MLayout>
--- packages/docs/src/components/index.js
+++ packages/docs/src/components/index.js
@@ -2,7 +2,8 @@ import Avatars from './Avatars'
 import Buttons from './Buttons'
 import Cards from './Cards'
 import Chips from './Chips'
+import Icons from './Icons'
 
-const components = [Avatars, Buttons, Cards, Chips]
+const components = [Avatars, Buttons, Cards, Chips, Icons]
 
 export default components
