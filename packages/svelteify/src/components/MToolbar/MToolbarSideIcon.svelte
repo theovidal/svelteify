d427e99bfea827ec896a405a2fc12e83953c56bf
Exybore <exybore@becauseofprog.fr>
2019-09-01 18:08:29 +0200

    feat: add classes and light props to MToolbarSideIcon

--- packages/svelteify/src/components/MToolbar/MToolbarSideIcon.svelte
+++ packages/svelteify/src/components/MToolbar/MToolbarSideIcon.svelte
@@ -1,18 +1,24 @@
 <script>
   import { createEventDispatcher } from 'svelte'
+  import { theme } from '../../stores/theme'
   import { MBtn } from '../MBtn'
 
+  export let classes = ''
   export let dark = false
+  export let light = false
 
-  const dispatch = createEventDispatcher()
+  // Theme-related operations
+  let darkTheme = false
 
-  function dispatchClick(event) {
-    dispatch('click', {
-      event
-    })
+  $: if (dark) {
+    darkTheme = true
+  } else if (light) {
+    darkTheme = false
+  } else {
+    darkTheme = $theme.dark
   }
 </script>
 
-<MBtn {dark} classes="v-toolbar__side-icon" icon on:click="{dispatchClick}">
+<MBtn {dark} classes="v-toolbar__side-icon {classes}" icon on:click>
   <i aria-hidden="true" class="v-icon material-icons theme--light">menu</i>
 </MBtn>
