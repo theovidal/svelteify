a83d7811b04a8c43f60131baf22a3955bcf0cb6e
Exybore <exybore@becauseofprog.fr>
2019-09-02 21:56:37 +0200

    feat: add drawer reactivity
    Added reactivity for the navigation drawer (MNavigationDrawer
    component), so it can dynamically adjust its size and visibility using
    properties and breakpoints.
    
    TODO : animate drawer opening / closing

--- packages/svelteify/src/components/MNavigationDrawer/MNavigationDrawer.svelte
+++ packages/svelteify/src/components/MNavigationDrawer/MNavigationDrawer.svelte
@@ -14,17 +14,18 @@
   export let height = 'auto'
   export let light = false
   export let mini = false
+  export let mini_width = '80px'
+  export let mobile_breakpoint = 1264
+  export let permanent = false
   export let right = false
+  export let temporary = false
+  export let show = false
   export let width = '300px'
-
-  if (mini) {
-    width = '80px'
-  }
 
   // Theme-related operations
   let { style, strColors } = generateTheme($theme, color, true)
 
-  let darkTheme = false
+  let darkTheme
 
   $: if (dark) {
     darkTheme = true
@@ -33,20 +34,59 @@
   } else {
     darkTheme = $theme.dark
   }
+
+  // Displaying-related operations
+  let display
+  let drawerWidth
+  let permanentShow = true
+
+  $: if (mini) {
+    drawerWidth = mini_width
+  } else {
+    drawerWidth = width
+  }
+
+  window.onload = refreshShow
+  window.onresize = refreshShow
+
+  show = permanent ? true : show
+
+  $: {
+    display = show ? 'block' : 'none'
+  }
+
+  function refreshShow() {
+    permanentShow = window.innerWidth >= mobile_breakpoint
+    if (!permanent && !temporary) {
+      show = permanentShow
+    }
+  }
+
+  function closeDrawer() {
+    show = false
+  }
 </script>
+
+{#if !permanentShow && show}
+<div class="v-overlay v-overlay--active" on:click="{closeDrawer}"></div>
+{/if}
 
 <aside
   id="{app ? 'app-drawer' : ''}"
   class="v-navigation-drawer {strColors} {classes}"
   class:v-navigation-drawer--absolute="{absolute}"
   class:v-navigation-drawer--clipped="{clipped}"
+  class:v-natigation-drawer--close="{!show}"
   class:v-navigation-drawer--fixed="{fixed}"
   class:v-navigation-drawer--floating="{floating}"
+  class:v-navigation-drawer--is-mobile="{!permanentShow}"
   class:v-navigation-drawer--mini-variant="{mini}"
+  class:v-navigation-drawer--open="{show}"
   class:v-navigation-drawer--right="{right}"
+  class:v-navigation-drawer--temporary="{temporary}"
   class:theme--dark="{darkTheme}"
   class:theme--light="{!darkTheme}"
-  style="width: {width}; height: {height}; {style}"
+  style="display: {display}; height: {height}; width: {drawerWidth}; {style}"
 >
   <slot />
 </aside>
